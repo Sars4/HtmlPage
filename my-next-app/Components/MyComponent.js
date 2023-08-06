35ca2609b8fcbc6287e68e4c2928eccc2c1476c7
Sars4 <logansars225@gmail.com>
2023-08-06 17:45:17 -0400

    nextjs update

---
+++ my-next-app/Components/MyComponent.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from '../styles/mystyles.module.css';
+
+const MyComponent = () => {
+  return (
+    <div>
+    <head>
+    <title>Logan Sarsfield</title>
+  
+    </head>
+      <h1>Hello World</h1>
+      <br></br>
+
+    <main>
+      <p>My name is Logan Sarsfield, and I am a senior at Indiana University majoring in computer science with a focus in data science, and minoring in marketing.
+      I 
+      </p>
+      <hr></hr>
+    </main>
+    </div>
+  );
+};
+
+export default MyComponent;
